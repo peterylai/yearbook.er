9ea7e2f5fbcd952a6134972f40d5bd460bebaedd
Peter Lai <peter.yd.lai@gmail.com>
2014-03-17 11:25:41 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yearbooker::Application.config.secret_key_base = 'b9143480c91167759d19b093bff6a14e50129c6b949feee49e31960cbda8650da2b8af7ea449a02fa0c4313c97794038068f89dbaf8e1d97c0e6504f9649a1d2'
