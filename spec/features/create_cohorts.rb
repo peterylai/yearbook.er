ac14fb17e3f6429c9f53ca255e1d4fdc5734a0ff
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-17 14:13:06 -0400

    user adds class feature

--- spec/features/create_cohorts.rb
+++ spec/features/create_cohorts.rb
@@ -7,11 +7,11 @@ describe 'creating a cohort' do
     visit "/cohorts/new"
     # save_and_open_page
     fill_in 'cohort[location]', :with => "New York City"
-    fill_in 'Month:', :with => 'January'
-    fill_in 'Year:', :with => '2014'
-    fill_in 'Nickname:', :with => '2014 Karaoke All-Stars'
-
-    click_button 'Add Cohort'
+    fill_in 'cohort[month]', :with => 'January'
+    fill_in 'cohort[year]', :with => '2014'
+    fill_in 'cohort[nickname]', :with => '2014 Karaoke All-Stars'
+    click_button 'Submit'
+    save_and_open_page
     expect(page).to have_content 'New York City'
     expect(page).to have_content 'January, 2014'
 
