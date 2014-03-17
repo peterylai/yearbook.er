2bc6e1cf22eda994a85d8e6bf4d4f94d78611aa9
Peter Lai <peter.yd.lai@gmail.com>
2014-03-17 16:21:41 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Yearbooker::Application.routes.draw do
   resources :cohorts, only: [:new, :create, :show]
+  resources :campuses, only: [:new, :create, :show]
 
   get "/signup", to: "users#new"
   post "/users", to: "users#create"
