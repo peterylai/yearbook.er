bcf7adcb74ce95c854dd686d8e96b8607d7b5a57
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-18 14:52:55 -0400

    unit tests for users, added is_admin function

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,12 +5,19 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password "pizza"
     password_confirmation "pizza"
-    profile_pic { Faker::Internet.http_url  }
+    # profile_pic { Faker::Internet.http_url  }
     github { Faker::Internet.http_url }
     linkedin { Faker::Internet.http_url }
     bio { Faker::Lorem.paragraph }
     website { Faker::Internet.http_url }
     employer { Faker::Company.name }
+  end
 
+  factory :admin, class: Admin, parent: :user do 
+    type "Admin"
+  end
+
+  factory :student, class: Student, parent: :user do 
+    type "Student"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ it { should validate_presence_of (:email)}
 it { should validate_presence_of(:first_name)}
 it { should validate_presence_of(:last_name)}
 
+  describe Admin do
+    
+    let!(:admin) { FactoryGirl.create(:admin) }
+    let!(:student) { FactoryGirl.create(:student) }
+
+    describe "#is_admin?" do
+      it "returns whether a user is an admin" do
+        expect(admin.is_admin?).to be_true
+        expect(student.is_admin?).to be_false
+      end
+    end
+  end
 end
