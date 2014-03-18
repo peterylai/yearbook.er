43a9757802e5164729eb0a3f347300dcbd341c31
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-18 11:20:15 -0400

    commit

--- spec/factories/cohort.rb
+++ spec/factories/cohort.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
   factory :cohort do 
    month { Faker::Time.month }
-   year { Faker::Time.year }
+   year { Faker::Time.date[0..3] }
    nickname { Faker::DizzleIpsum.word }
    association :campus
   end
